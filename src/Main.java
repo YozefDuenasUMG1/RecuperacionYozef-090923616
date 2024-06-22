d3ede0f2c90adb1d8cbe3a961d012f987335417d
unknown <yduenass2@miumg.edu.gt>
2024-06-22 10:37:22 -0600

    segundocommitcalculadora

--- src/Main.java
+++ src/Main.java
@@ -32,3 +32,40 @@ class ExamenFinalVarianteA {
 
         scanner.close();
     }
+
+    public static void realizarOperacion() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el primer número: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Ingrese el segundo número: ");
+        double num2 = scanner.nextDouble();
+        System.out.print("Ingrese el operador (+, -, *, /): ");
+        char operador = scanner.next().charAt(0);
+
+        double resultado = 0;
+        switch (operador) {
+            case '+':
+                resultado = num1 + num2;
+                break;
+            case '-':
+                resultado = num1 - num2;
+                break;
+            case '*':
+                resultado = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    resultado = num1 / num2;
+                } else {
+                    System.out.println("Error: División por cero.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Operador no válido.");
+                return;
+        }
+
+        System.out.println("Resultado: " + resultado);
+    }
+
