4af3bcfd1afcc593b7cd13ef8116128de949c98a
unknown <yduenass2@miumg.edu.gt>
2024-06-22 10:40:08 -0600

    cuartocommit

--- src/Main.java
+++ src/Main.java
@@ -151,7 +151,22 @@ class Estudiante {
         this.nombre = nombre;
         this.calificacion = calificacion;
     }
+    public String getNombre() {
+        return nombre;
+    }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(double calificacion) {
+        this.calificacion = calificacion;
+    }
+}
 
 
 
